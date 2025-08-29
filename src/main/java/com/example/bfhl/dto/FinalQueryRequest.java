c228f907ca44bf1af0a356112d8709ce4e7f2859
Rishitha Errepalli <153429856+rishwGit@users.noreply.github.com>
2025-08-29 11:52:02 +0530

    Create FinalQueryRequest.java

---
+++ src/main/java/com/example/bfhl/dto/FinalQueryRequest.java
@@ -0,0 +1,8 @@
+package com.example.bfhl.dto;
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class FinalQueryRequest {
+    private String finalQuery;
+}
